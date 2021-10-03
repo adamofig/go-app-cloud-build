ca306e120baa37c29af91693bcbc420e2d2023ed
Adamo Figueroa <adamo.figueroa@gmail.com>
2021-10-02 19:16:58 -0500

    cambio malo

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(r *gin.Context) {
+	r.GET("/", func(*gin.Context) {
 		html := `
 
 		<body style="background-color: #2ffC62; color: white; text-align: center; padding: 200px;">
